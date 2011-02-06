c9e233f2366f3eed52931a293650acf475081b09
Ronald Oussoren <ronaldoussoren@mac.com>
2011-02-06 21:47:23 +0100

    Add placeholders for tests for altgraph.Graph

--- altgraph_tests/test_graph.py
+++ altgraph_tests/test_graph.py
@@ -12,6 +12,8 @@ class TestGraph (unittest.TestCase):
     def test_nodes(self):
         graph = Graph()
 
+        self.assertEqual(graph.node_list(), [])
+
         o1 = object()
         o1b = object()
         o2 = object()
@@ -56,6 +58,66 @@ class TestGraph (unittest.TestCase):
         self.assertTrue(1 in graph)
         self.assertTrue(2 in graph)
         self.assertTrue(3 in graph)
+
+        self.assertEqual(list(sorted(graph.node_list())), [1, 2, 3])
+
+        v = graph.describe_node(1)
+        self.assertEqual(v, (1, o1, [], []))
+
+    @unittest.expectedFailure
+    def test_edges(self):
+        self.fail("add basic tests for graph with edges")
+
+    @unittest.expectedFailure
+    def test_toposort(self):
+        self.fail("add tests for forw_topo_sort and back_topo_sort")
+
+    @unittest.expectedFailure
+    def test_bfs_subgraph(self):
+        self.fail("add tests for forw_bfs_subgraph and back_bfs_subgraph")
+
+    @unittest.expectedFailure
+    def test_iterdfs(self):
+        self.fail("add tests for iterdfs")
+
+    @unittest.expectedFailure
+    def test_iterdata(self):
+        self.fail("add tests for iterdata")
+
+    @unittest.expectedFailure
+    def test_bfs(self):
+        self.fail("add tests for forw_bfs and back_bfs")
+
+    @unittest.expectedFailure
+    def test_dfs(self):
+        self.fail("add tests for forw_dfs and back_dfs")
+
+    def test_connected(self):
+        graph = Graph()
+        graph.add_node(1)
+        graph.add_node(2)
+        graph.add_node(3)
+        graph.add_node(4)
+
+        self.assertFalse(graph.connected())
+
+        graph.add_edge(1, 2)
+        graph.add_edge(3, 4)
+        self.assertFalse(graph.connected())
+
+        graph.add_edge(2, 3)
+        graph.add_edge(4, 1)
+        self.assertTrue(graph.connected())
+
+    @unittest.expectedFailure
+    def test_clust_coef(self):
+        self.fail("add tests for clust_coef")
+
+    @unittest.expectedFailure
+    def test_get_hops(self):
+        self.fail("add tests for get_hops")
+
+
 
 if __name__ == "__main__":
     unittest.main()
